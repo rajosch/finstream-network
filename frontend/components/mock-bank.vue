7e46a63e114e8e06015e0e1d67681b7d63941f2a
Raphael Schmid <raphaeljschmid@protonmail.com>
2024-06-21 11:00:08 -0600

    commit

--- frontend/components/mock-bank.vue
+++ frontend/components/mock-bank.vue
@@ -113,7 +113,7 @@
                 <div class="flex justify-between items-center">
                   <img
                     src="/img/icons/money_sent.png"
-                    class="h-6 w-6 rotate-180"
+                    class="h-5 w-5 rotate-180"
                   > 
                   <div>
                     {{ transaction.currencySent }}{{ transaction.amountSent.toFixed(2) }}
@@ -121,18 +121,19 @@
                   <img
                     v-if="transaction.status === 'completed'"
                     src="/img/icons/completed.png"
-                    class="h-6 w-6"
+                    class="h-4 w-4"
                   > 
                   <img
                     v-else-if="transaction.status === 'failed'"
                     src="/img/icons/failed.png"
-                    class="h-6 w-6"
+                    class="h-4 w-4"
                   > 
-                  <img
-                    v-else
-                    src="/img/icons/pending.png"
-                    class="h-2 w-5"
-                  > 
+                  <span v-else class="flex items-center justify-center">
+                    <svg class="animate-spin h-4 w-4 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                  </span>
                 </div>
               </div>
               <div
@@ -143,7 +144,7 @@
                 <div class="flex justify-between items-center">
                   <img
                     src="/img/icons/money_received.png"
-                    class="h-6 w-6"
+                    class="h-5 w-5"
                   > 
                   <div>
                     {{ transaction.currencyReceived }}{{ transaction.amountReceived.toFixed(2) }}
@@ -151,18 +152,19 @@
                   <img
                     v-if="transaction.status === 'completed'"
                     src="/img/icons/completed.png"
-                    class="h-6 w-6"
+                    class="h-4 w-4"
                   > 
                   <img
                     v-else-if="transaction.status === 'failed'"
                     src="/img/icons/failed.png"
-                    class="h-6 w-6"
+                    class="h-4 w-4"
                   > 
-                  <img
-                    v-else
-                    src="/img/icons/pending.png"
-                    class="h-2 w-5"
-                  > 
+                  <span v-else class="flex items-center justify-center">
+                    <svg class="animate-spin h-4 w-4 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                  </span>
                 </div>
               </div>
             </div>
